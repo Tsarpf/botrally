ed8b25686a0c7990a28790dc835a82691c76c3c5
Teemu Sarapisto <tsarpf@gmail.com>
2018-04-18 01:52:25 +0300

    Even moar tutorial

--- client/src/react/selection.jsx
+++ client/src/react/selection.jsx
@@ -7,6 +7,7 @@ const driverSelection = {
 
 const tutorial = [
 'keyboard left/right arrow to turn when human selected, changes to driver from dropdown only affect next game.',
+'game starts when there are 2 players available (so open a second tab if you have no friends). one defaultbot is always added',
 'game autorestarts at 60 seconds if no winners',
 'there is a one second wait at beginning of the game',
 'to add new bots: !+addbot botname <bot source code>. After a page refresh the bot is selectable from the dropdown menu',
